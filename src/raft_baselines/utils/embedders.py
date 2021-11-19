61988265ad6681e200daec3e3402c8d1a75894bc
VictorSanh <victorsanh@gmail.com>
2021-11-19 21:00:00 +0000

    hack arround to avoid using internet when not needed for sentence transformer

--- src/raft_baselines/utils/embedders.py
+++ src/raft_baselines/utils/embedders.py
@@ -2,7 +2,8 @@ from abc import ABC, abstractmethod
 from typing import List, Tuple
 from sentence_transformers import SentenceTransformer
 import torch
-
+import requests
+import os
 
 class Embedder(ABC):
     @abstractmethod
@@ -15,7 +16,16 @@ class SentenceTransformersEmbedder(Embedder):
         self, model_name="sentence-transformers/all-MiniLM-L6-v2", max_seq_length=512
     ):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.similarity_model = SentenceTransformer(model_name, device=self.device)
+        try:
+            self.similarity_model = SentenceTransformer(model_name, device=self.device)
+        except requests.exceptions.ProxyError:
+            model_path = os.path.join(os.getenv("SENTENCE_TRANSFORMERS_HOME"), model_name.replace("/", "_"))
+            if os.path.exists(os.path.join(model_path, "modules.json")):
+                dummy_class = SentenceTransformer()
+                modules = dummy_class._load_sbert_model(model_path)
+            else:
+                raise ValueError(f"{os.path.join(model_path, 'modules.json')} does not exist")
+            self.similarity_model = SentenceTransformer(modules=modules, device=self.device)
         self.similarity_model.max_seq_length = max_seq_length
         self._cache = {}
 
